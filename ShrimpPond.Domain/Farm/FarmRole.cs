d48f9a4c16f704e5a66a969e096708f1d60f10f4
vanvu0211 <vu34304@gmail.com>
2025-05-13 23:47:19 +0700

    new branch

---
+++ ShrimpPond.Domain/Farm/FarmRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrimpPond.Domain.Farm
+{
+    public class FarmRole
+    {
+        public string Email { get; set; } = string.Empty;
+        public bool IsAdmin { get; set; }
+    }
+}
